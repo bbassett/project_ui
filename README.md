48721b237d8d348d90a3bc4f0b2672fc8691976a
Brandon Bassett <bassett.brandon@gmail.com>
2016-12-06 22:56:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# project_ui
+
+generic app as starter
+
+## Running
+
+```sh
+git clone https://github.com/brandon/project_ui
+cd project_ui
+npm i
+make
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,35 @@
+__webpack_public_path__ = __build_path__;
+/**
+ * Module dependencies
+ */
+
+import Poe from 'poe-ui';
+import Forms from 'form-store';
+import Store from 'hyper-store';
+import Client from 'hyper-client-wait1';
+import Format from 'hyper-uri-format';
+import Translate from 'onus-translate';
+import routes from 'onus-router/react?enhancers=basename!../web';
+
+var API_URL = browser.env.API_URL;
+
+/**
+ * Instantiate dependencies
+ */
+
+var format = require('hyper-uri-format')(API_URL);
+var client = new Client(API_URL);
+var store = new Store(client);
+var forms = new Forms(client);
+
+module.exports = Poe(document.getElementById('app'), {
+  router: {
+    activeLinkClassName: 'is-active',
+    basename: __app_path__,
+    routes: routes
+  },
+  format: format,
+  forms: new Forms(client),
+  store: store,
+  translate: new Translate('.translations.project_ui')
+});
